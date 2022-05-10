51f928a41899035cdc3d149a3bdc02cbd1ff5ad4
AlinaRskv <rusakovichalina@gmail.com>
2022-05-11 02:08:08 +0300

    Create train.py

---
+++ src/forest_ml/train.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from joblib import dump
+
+import click
+import mlflow
+import mlflow.sklearn
+from sklearn.metrics import accuracy_score
+
+from .data import get_dataset
+from .pipeline import create_pipeline
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--test-split-ratio",
+    default=0.3,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
+@click.option(
+    "--use-scaler",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+
+@click.option(
+    "--k",
+    default=5,
+    type=int,
+    show_default=True,
+)
+
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+def train(
+    dataset_path: Path,
+    save_model_path: Path,
+    test_split_ratio: float,
+    use_scaler: bool,
+    k: int,
+    random_state: int,
+) -> None:
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
+    )
+    with mlflow.start_run():
+        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
+        pipeline.fit(features_train, target_train)
+        accuracy = accuracy_score(target_val, pipeline.predict(features_val))
+        mlflow.log_param("use_scaler", use_scaler)
+        mlflow.log_param("k", k)
+        mlflow.log_metric("accuracy", accuracy)
+        click.echo(f"Accuracy: {accuracy}.")
+        dump(pipeline, save_model_path)
+        click.echo(f"Model is saved to {save_model_path}.")
