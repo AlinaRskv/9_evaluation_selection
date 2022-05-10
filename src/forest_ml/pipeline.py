3b690f715579fba4290aba31205200ab1f585538
AlinaRskv <rusakovichalina@gmail.com>
2022-05-11 02:00:49 +0300

    Create pipeline.py

---
+++ src/forest_ml/pipeline.py
@@ -0,0 +1,20 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def create_pipeline(
+    use_scaler: bool, k: float
+) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+    pipeline_steps.append(
+        (
+            "classifier",
+            KNeighborsClassifier(
+                n_neighbors=k
+            ),
+        )
+    )
+    return Pipeline(steps=pipeline_steps)
