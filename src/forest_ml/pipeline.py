e306693f5527843c5a671752b6ffb1c177c8bf7d
AlinaRskv <rusakovichalina@gmail.com>
2022-05-11 15:34:38 +0300

    Add Decision Tree model

--- src/forest_ml/pipeline.py
+++ src/forest_ml/pipeline.py
@@ -1,22 +1,38 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 
 def create_pipeline(
-    use_scaler: bool, k: int, weights: str, algorithm: str
+    use_scaler: bool, scaler: str,  k: int, weights: str, p: int, model: str, criterion: str, max_depth: int
 ) -> Pipeline:
     pipeline_steps = []
-    if use_scaler:
+    if use_scaler and scaler == 'StandardScaler':
         pipeline_steps.append(("scaler", StandardScaler()))
-    pipeline_steps.append(
-        (
-            "classifier",
-            KNeighborsClassifier(
-                n_neighbors=k,
-                weights=weights,
-                algorithm=algorithm
-            ),
+    elif use_scaler and scaler == 'MinMaxScaler':
+        pipeline_steps.append(("scaler", MinMaxScaler()))
+    if model == 'KNeighbors':
+        pipeline_steps.append(
+            (
+                "classifier",
+                KNeighborsClassifier(
+                    n_neighbors=k,
+                    weights=weights,
+                    p=p
+                ),
+            )
         )
-    )
+    elif model == 'DecisionTree':
+        pipeline_steps.append(
+            (
+                "classifier",
+                DecisionTreeClassifier(
+                    random_state=42,
+                    criterion=criterion,
+                    max_depth=max_depth
+                ),
+            )
+        )
     return Pipeline(steps=pipeline_steps)
